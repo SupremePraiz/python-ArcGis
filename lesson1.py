0d9faa92cb5582ed95f16b7c9934bc6998928465
Supremepraiz <supremepraiz2@gmail.com>
2024-05-17 22:59:25 +0100

    added arcpy

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,12 @@
 from time import ctime
+import arcpy
 
+print(f"the start time and date is {ctime()}")
+
+
+fc = r"C:\Users\Supremepraiz\Desktop\LPC\Data\ne_10m_admin_0_countries.shp"
+
+count = arcpy.GetCount_management(fc)
+print(count)
 
 print(f"the current time and date is {ctime()}")
