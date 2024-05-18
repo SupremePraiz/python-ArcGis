29593105b9ee27fe86bfee33d6d9a3dc13213116
Supremepraiz <supremepraiz2@gmail.com>
2024-05-18 15:35:38 +0100

    added more arcpy module1

--- lesson1.py
+++ lesson1.py
@@ -4,13 +4,20 @@ import arcpy
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = r"C:\Users\Supremepraiz\Desktop\LPC\Data\sample.gdb"
 
-fc = r"C:\Users\Supremepraiz\Desktop\LPC\Data\ne_10m_admin_0_countries.shp"
+# fc = r"C:\Users\Supremepraiz\Desktop\LPC\Data\ne_10m_admin_0_countries.shp"
+fc = arcpy.GetParameterAsText(0)
+
+if fc == "":
+    fc = r"C:\Users\Supremepraiz\Desktop\LPC\Data\ne_10m_admin_0_countries.shp"
+
 
 num_feature = arcpy.GetCount_management(fc)
-print("this shape file {0} has {1} in it".format(fc,num_feature))
+arcpy.AddMessage("this shape file {0} has {1} in it".format(fc,num_feature))
 
-arcpy.CreateFileGDB_management(r"C:\Users\Supremepraiz\Desktop\LPC\Data","sample")
-arcpy.Select_analysis(fc, "Canada", "NAME = 'Canada'")
+# arcpy.CreateFileGDB_management(r"C:\Users\Supremepraiz\Desktop\LPC\Data","sample")
+# arcpy.Select_analysis(fc, "Canada", "NAME = 'Canada'")
+
+
 
 print("...........................................................................................")
 print("Script Completed")
