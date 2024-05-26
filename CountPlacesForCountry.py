a7324cecca78a36cdbbf1b987e4431d4b852e113
Supremepraiz <supremepraiz2@gmail.com>
2024-05-26 15:25:07 +0100

    added correction to CountPlaces module1

--- CountPlacesForCountry.py
+++ CountPlacesForCountry.py
@@ -1,14 +1,19 @@
 import arcpy
 
 arcpy.env.overwriteOutput = True
+arcpy.env.workspace = r"C:\Users\Supremepraiz\Documents\ArcGIS\Projects\learning_buffering\learning_buffering.gdb"
+
+country_name = "China"
+
 arcpy.analysis.Select(
-    in_features="Countries",
+    in_features= r"C:\Users\Supremepraiz\Desktop\LPC\Data\ne_10m_admin_0_countries.shp",
     out_feature_class=r"C:\Users\Supremepraiz\Documents\ArcGIS\Projects\learning_buffering\learning_buffering.gdb\Countries_Select",
-    where_clause="NAME = 'India'"
+    where_clause="NAME = '{0}'".format(country_name)
 )
+
 arcpy.analysis.Buffer(
     in_features="Countries_Select",
-    out_feature_class=r"C:\Users\Supremepraiz\Documents\ArcGIS\Projects\learning_buffering\learning_buffering.gdb\Countries_Select_Buffer",
+    out_feature_class="Countries_Select_Buffer",
     buffer_distance_or_field="200 Kilometers",
     line_side="FULL",
     line_end_type="ROUND",
@@ -17,16 +22,15 @@ arcpy.analysis.Buffer(
     method="PLANAR"
 )
 arcpy.analysis.Clip(
-    in_features="Places",
+    in_features=r"C:\Users\Supremepraiz\Desktop\LPC\Data\ne_10m_populated_places.shp",
     clip_features="Countries_Select_Buffer",
     out_feature_class=r"C:\Users\Supremepraiz\Documents\ArcGIS\Projects\learning_buffering\learning_buffering.gdb\Places_Clip",
     cluster_tolerance=None
 )
-arcpy.management.GetCount(
-    in_rows="Places_Clip"
-)
-arcpy.management.GetCount(
-    in_rows="Places_Clip"
-)
-arcpy.management.GetCount("Places_Clip")
+
+print("there's {0} populated places in and within 200k of {1}".format(arcpy.management.GetCount("Places_Clip"),country_name))
+
 # <Result '119'>
+
+
+print("Script ran Successfully")
